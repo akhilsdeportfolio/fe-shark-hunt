88b53f26e1a82f3d7d3d0583e89ae30cc6e67c89
Akhil Kamsala <87219018+akhilsdeportfolio@users.noreply.github.com>
2024-10-12 11:00:24 +0530

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/MPUNqvwxyI

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'shark-hunt',
+  preset: './jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage/shark-hunt',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
